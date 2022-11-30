b63300b64e91a4fbb969c3231366bc5dbf92ebf7
Luis Machado Reis <luis.reis@trustly.com>
2022-11-29 22:34:29 -0300

    Adding Jacoco to the Project

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# java-for-juniors-6-using-junit5
+# java-for-juniors-7-using-junit5
 
-Live Coding at Youtube [here](https://www.youtube.com/watch?v=tga2TzN_KBw)!
+Live Coding at YouTube [here](https://www.youtube.com/watch?v=tga2TzN_KBw)!
--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,6 +2,9 @@ import data.Customer;
 
 public final class HelloWorld {
 
+    private HelloWorld() {
+    }
+
     public static void main(String[] args) {
         Customer c = new Customer(1L, "Luis");
         System.out.println("Hello %s! You are the customer number #%d".formatted(c.getName(), c.getId()));
